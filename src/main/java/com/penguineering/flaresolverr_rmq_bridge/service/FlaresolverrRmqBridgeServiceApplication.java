3be37a6fe036cb0c7ab7ec7bef3f114b5505977d
Stefan Haun <mail@tuxathome.de>
2025-02-21 15:59:27 +0100

    Add project generated by Spring Initializr
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.3&packaging=jar&jvmVersion=21&groupId=com.penguineering.flaresolverr-rmq-bridge&artifactId=flaresolverr-rmq-bridge-service&name=flaresolverr-rmq-bridge-service&description=Post%20requests%20to%20FlareSolverr%20via%20RabbitMQ&packageName=com.penguineering.flaresolverr-rmq-bridge.service&dependencies=lombok,webflux,amqp,actuator

---
+++ src/main/java/com/penguineering/flaresolverr_rmq_bridge/service/FlaresolverrRmqBridgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.penguineering.flaresolverr_rmq_bridge.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaresolverrRmqBridgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaresolverrRmqBridgeServiceApplication.class, args);
+	}
+
+}
